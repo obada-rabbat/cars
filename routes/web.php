3f8a16f02bded3bc898dfb39c44e452fd32c196a
obada-rabbat <obadarbt5@hotmail.com>
2020-11-18 20:33:15 +0200

    edite

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@
 
 
 // PagesController Routs
-Route::get('/','PagesController@index')->name('home');
-Route::get('/home','PagesController@index')->name('home');
+Route::get('/','PagesController@index')->name('hom');
+Route::get('/home','PagesController@index')->name('hom');
 Route::get('/404','PagesController@eror')->name('eror');
 Route::get('/about_us','PagesController@about_us')->name('about-us');
 Route::get('/blog_home','PagesController@blog_home')->name('blog-home');
@@ -47,7 +47,7 @@ Route::get('/withdraw','PagesController@withdraw')->name('withdraw');
 
 Auth::routes();
 
-Route::get('/login', 'HomeController@index')->name('login');
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 // CountryController
